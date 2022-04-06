294b28a8dbd126a8e9111f01d8ddd4ee6242d97f
andrewscarlos <andrews.knct@gmail.com>
2022-04-05 21:21:44 -0300

    feat: add transactions

--- domain/model/account.go
+++ domain/model/account.go
@@ -1,37 +1,46 @@
 package model
 
 import (
-	"github.com/asaskevish/govalidator"
-	uuid "github.com/satori/go.uuid"
 	"time"
+
+	"github.com/asaskevich/govalidator"
+	uuid "github.com/satori/go.uuid"
 )
-type Account struct {
-	Base `valid:"required"`
-	OwnerName string `json:"owner_name" valid"notnull"`
-	Bank *Bank `valid:"-"`
-	Number string `json:"number" valid:"notnull"`	
+
+func init() {
+	govalidator.SetFieldsRequiredByDefault(true)
 }
 
-func (account *Account) isValid() error{
-	_, erro := govalidator.ValidateStruct(account)
-	if err != nil{
+type Account struct {
+	Base      `valid:"required"`
+	OwnerName string    `gorm:"column:owner_name;type:varchar(255);not null" valid:"notnull"`
+	Bank      *Bank     `valid:"-"`
+	BankID    string    `gorm:"column:bank_id;type:uuid;not null" valid:"-"`
+	Number    string    `json:"number" gorm:"type:varchar(20)" valid:"notnull"`
+	PixKeys   []*PixKey `gorm:"ForeignKey:AccountID" valid:"-"`
+}
+
+func (account *Account) isValid() error {
+	_, err := govalidator.ValidateStruct(account)
+	if err != nil {
 		return err
 	}
 	return nil
 }
 
-func NewAccount(bank *Bank, number string, OwnerName string)(*Account, error){
+func NewAccount(bank *Bank, number string, ownerName string) (*Account, error) {
 	account := Account{
+		Bank:      bank,
+		BankID:    bank.ID,
+		Number:    number,
 		OwnerName: ownerName,
-		Bank : bank,
-		Number:     number
 	}
 
 	account.ID = uuid.NewV4().String()
 	account.CreatedAt = time.Now()
 
-	err := account.IsValid()
-	if err != nil{
+	err := account.isValid()
+	if err != nil {
 		return nil, err
 	}
 	return &account, nil
--- domain/model/base.go
+++ domain/model/base.go
@@ -1,14 +1,18 @@
+
 package model
 
+import (
+	"time"
 
-import "time"
+	"github.com/asaskevich/govalidator"
+)
 
-func init(){
+func init() {
 	govalidator.SetFieldsRequiredByDefault(true)
 }
 
 type Base struct {
-	ID string `json."id" valid:"uuid"`
+	ID        string    `json:"id" gorm:"type:uuid;primary_key" valid:"uuid"`
 	CreatedAt time.Time `json:"created_at" valid:"-"`
 	UpdatedAt time.Time `json:"updated_at" valid:"-"`
 }
